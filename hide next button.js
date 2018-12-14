304101674c945228ba0c8da79b611518a07f3210
Andras Molnar <45248738+andras-molnar@users.noreply.github.com>
2018-12-14 02:04:51 -0500

    Update hide next button.js

--- hide next button.js
+++ hide next button.js
@@ -1,11 +1,9 @@
-// this script
+// This script hides the next button and lets the participant automatically proceed after selecting a response (works only with single choice options)
 
 var that = this;
-	
-	this.hideNextButton();
-
-	this.questionclick = function(event,element){
-		if (element.type == 'radio')  {
-		   that.clickNextButton();
-		}
+this.hideNextButton();
+this.questionclick = function(event,element){
+	if (element.type == 'radio')  {
+		that.clickNextButton();
 	}
+}
