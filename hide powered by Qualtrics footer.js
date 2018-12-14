302e9b1f8b5fea9a30c591b4f25d87df5705e5b1
Andras Molnar <45248738+andras-molnar@users.noreply.github.com>
2018-12-14 02:21:23 -0500

    Update hide powered by Qualtrics footer.js

--- hide powered by Qualtrics footer.js
+++ hide powered by Qualtrics footer.js
@@ -1,7 +1,7 @@
 /* Add this line to any question on a page to hide the "Powered by Qualtrics" footer on that page: */
 document.getElementById("Plug").remove();
 
-/* Add this in the header or footer (survey look) to remove "Powered by Qualtrics" from the whole survey: */
+/* Add this to the header or footer (survey look) to remove "Powered by Qualtrics" from the whole survey: */
 <script>
 Qualtrics.SurveyEngine.addOnload(function()
 {
