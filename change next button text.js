be8bfaffe6729a8a3dfd9cd6e9d493aef0397bac
Andras Molnar <45248738+andras-molnar@users.noreply.github.com>
2018-12-14 02:18:19 -0500

    Create change next button text.js

---
+++ change next button text.js
@@ -0,0 +1,5 @@
+/* This script allows you to change the default next button text for a single page,
+without having to change the next button for the whole block.
+
+var nextButton = document.getElementsByClassName("NextButton Button")[0];
+nextButton.value = "Custom next button text"; // Put the custom text in the parentheses here
